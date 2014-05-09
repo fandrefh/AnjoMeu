854a4d592809c9aae1d04f165f71c3f1fda4e5bd
Francisco Andre <fandrefh@gmail.com>
2014-05-09 08:30:06 -0300

    Updated urls conf and views login redirect

--- anjo/core/urls.py
+++ anjo/core/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('anjo.core.views',
 	url(r'^como-funciona/$', 'howitwork', name='howwork'),
 	url(r'^anjos/$', 'donor', name='donors'),
 	url(r'^novo-usuario/$', 'register', name='newuser'),
-	url(r'^login/$', 'user_login', name='userlogin'),
+	url(r'^login/$', 'user_login', {'template_name': 'core/user_login.html'}),
 	url(r'^usuario-logado/$', 'user_logged', name='userlogged'),
 	url(r'^logout/$', 'user_logout', name='userlogout'),
 )
